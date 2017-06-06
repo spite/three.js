cfa56270ea40cf3b8eae396212922f10957cf983
Jaume Sanchez <the.spite@gmail.com>
2017-06-06 15:15:27 +0200

    restored regular image loading and added logic depending on detection

--- src/loaders/ImageLoader.js
+++ src/loaders/ImageLoader.js
@@ -56,97 +56,157 @@ function detect () {
 
 }
 
-let res = detect();
+let canUseImageBitmap = false;
+detect().then( res => { canUseImageBitmap = res; console.log( 'CI', res ) } );
 
 Object.assign( ImageLoader.prototype, {
 
 	load: function ( url, onLoad, onProgress, onError ) {
 
+		if( canUseImageBitmap ) this.loadImageBitmap( url, onLoad, onProgress, onError );
+		else this.loadImage( url, onLoad, onProgress, onError );
+
+	},
+
+	loadImage: function ( url, onLoad, onProgress, onError ) {
+
+		console.log( 'LOADING IMAGE', url );
+
 		if ( url === undefined ) url = '';
 
-			if ( this.path !== undefined ) url = this.path + url;
+		if ( this.path !== undefined ) url = this.path + url;
 
-			var scope = this;
+		var scope = this;
 
-			var cached = Cache.get( url );
+		var cached = Cache.get( url );
 
-			if ( cached !== undefined ) {
-
-				scope.manager.itemStart( url );
-
-				setTimeout( function () {
-
-					if ( onLoad ) onLoad( cached );
-
-					scope.manager.itemEnd( url );
-
-				}, 0 );
-
-				return cached;
-
-			}
-
-			fetch( url )
-				.then( res => res.blob() )
-				.then( res => createImageBitmap( res, { imageOrientation: 'flipY' } ) )
-				.then( res => {
-
-					Cache.add( url, res );
-
-					if ( onLoad ) onLoad( res );
-
-					scope.manager.itemEnd( url );
-
-				})
-				.catch( e => {
-
-					if ( onError ) onError( e );
-
-					scope.manager.itemEnd( url );
-					scope.manager.itemError( url );
-
-				});
-
-			var image = document.createElementNS( 'http://www.w3.org/1999/xhtml', 'img' );
-			/*
-			image.addEventListener( 'load', function () {
-
-				Cache.add( url, this );
-
-				if ( onLoad ) onLoad( this );
-
-				scope.manager.itemEnd( url );
-
-			}, false );
-
-			/*
-			image.addEventListener( 'progress', function ( event ) {
-
-				if ( onProgress ) onProgress( event );
-
-			}, false );
-			*/
-
-			/*image.addEventListener( 'error', function ( event ) {
-
-				if ( onError ) onError( event );
-
-				scope.manager.itemEnd( url );
-				scope.manager.itemError( url );
-
-			}, false );*/
-
-			if ( url.substr( 0, 5 ) !== 'data:' ) {
-
-				//if ( this.crossOrigin !== undefined ) image.crossOrigin = this.crossOrigin;
-
-			}
+		if ( cached !== undefined ) {
 
 			scope.manager.itemStart( url );
 
-			/*image.src = url;*/
+			setTimeout( function () {
 
-			return image;
+				if ( onLoad ) onLoad( cached );
+
+				scope.manager.itemEnd( url );
+
+			}, 0 );
+
+			return cached;
+
+		}
+
+		var image = document.createElementNS( 'http://www.w3.org/1999/xhtml', 'img' );
+
+		image.addEventListener( 'load', function () {
+
+			Cache.add( url, this );
+
+			if ( onLoad ) onLoad( this );
+
+			scope.manager.itemEnd( url );
+
+		}, false );
+
+		/*
+		image.addEventListener( 'progress', function ( event ) {
+			if ( onProgress ) onProgress( event );
+		}, false );
+		*/
+
+		image.addEventListener( 'error', function ( event ) {
+
+			if ( onError ) onError( event );
+
+			scope.manager.itemEnd( url );
+			scope.manager.itemError( url );
+
+		}, false );
+
+		if ( url.substr( 0, 5 ) !== 'data:' ) {
+
+			if ( this.crossOrigin !== undefined ) image.crossOrigin = this.crossOrigin;
+
+		}
+
+		scope.manager.itemStart( url );
+
+		image.src = url;
+
+		return image;
+
+	},
+
+	loadImageBitmap: function ( url, onLoad, onProgress, onError ) {
+
+		console.log( 'LOADING IMAGEBITMAP', url );
+
+		if ( url === undefined ) url = '';
+
+		if ( this.path !== undefined ) url = this.path + url;
+
+		var scope = this;
+
+		var cached = Cache.get( url );
+
+		if ( cached !== undefined ) {
+
+			scope.manager.itemStart( url );
+
+			setTimeout( function () {
+
+				if ( onLoad ) onLoad( cached );
+
+				scope.manager.itemEnd( url );
+
+			}, 0 );
+
+			return cached;
+
+		}
+
+		fetch( url )
+		.then( res => res.blob() )
+		.then( res => createImageBitmap( res, { imageOrientation: 'flipY' } ) )
+		.then( res => {
+
+			Cache.add( url, res );
+
+			if ( onLoad ) onLoad( res );
+
+			scope.manager.itemEnd( url );
+
+		})
+		.catch( e => {
+
+			if ( onError ) onError( e );
+
+			scope.manager.itemEnd( url );
+			scope.manager.itemError( url );
+
+		});
+
+		var image = document.createElementNS( 'http://www.w3.org/1999/xhtml', 'img' );
+
+		/*
+		image.addEventListener( 'progress', function ( event ) {
+
+			if ( onProgress ) onProgress( event );
+
+		}, false );
+		*/
+
+		if ( url.substr( 0, 5 ) !== 'data:' ) {
+
+			//if ( this.crossOrigin !== undefined ) image.crossOrigin = this.crossOrigin;
+
+		}
+
+		scope.manager.itemStart( url );
+
+		/*image.src = url;*/
+
+		return image;
 	},
 
 	setCrossOrigin: function ( value ) {
