023ea5dc281f262077b99175639499efed6cf67a
Jaume Sanchez <the.spite@gmail.com>
2017-06-08 01:26:07 +0200

    added check and image/imagebitmap loader to texture loader

--- src/loaders/TextureLoader.js
+++ src/loaders/TextureLoader.js
@@ -4,9 +4,45 @@
 
 import { RGBAFormat, RGBFormat } from '../constants';
 import { ImageLoader } from './ImageLoader';
+import { ImageBitmapLoader } from './ImageBitmapLoader';
 import { Texture } from '../textures/Texture';
 import { DefaultLoadingManager } from './LoadingManager';
 
+function detectCreateImageBitmap () {
+
+	var url = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==';
+
+	return new Promise( ( resolve, reject ) => {
+
+		if( !( 'createImageBitmap' in window ) ) {
+			reject();
+			return;
+		}
+
+		fetch( url )
+			.then( res => res.blob() )
+			.then( blob => {
+				console.log( 'blob is', blob);
+				Promise.all( [
+					createImageBitmap( blob, {imageOrientation: "none", premultiplyAlpha: "none"}),
+					createImageBitmap( blob, {imageOrientation: "flipY", premultiplyAlpha: "none"}),
+					createImageBitmap( blob, {imageOrientation: "none", premultiplyAlpha: "premultiply"}),
+					createImageBitmap( blob, {imageOrientation: "flipY", premultiplyAlpha: "premultiply"})
+				]).then( res => {
+					console.log( res );
+					resolve();
+				}).catch( e => {
+					console.log( e );
+					reject();
+				})
+
+			});
+
+	});
+
+}
+
+let canUseImageBitmap = detectCreateImageBitmap()
 
 function TextureLoader( manager ) {
 
@@ -18,28 +54,63 @@ Object.assign( TextureLoader.prototype, {
 
 	load: function ( url, onLoad, onProgress, onError ) {
 
-		var loader = new ImageLoader( this.manager );
-		loader.setCrossOrigin( this.crossOrigin );
-		loader.setPath( this.path );
-
 		var texture = new Texture();
-		loader.load( url, function ( img ) {
 
-			texture.image = img;
+		canUseImageBitmap
+		.then( () => {
 
-			// JPEGs can't have an alpha channel, so memory can be saved by storing them as RGB.
-			var isJPEG = url.search( /\.(jpg|jpeg)$/ ) > 0 || url.search( /^data\:image\/jpeg/ ) === 0;
+			console.log( 'USING IMAGE BITMAP' );
 
-			texture.format = isJPEG ? RGBFormat : RGBAFormat;
-			texture.needsUpdate = true;
+			var loader = new ImageBitmapLoader( this.manager );
+			loader.setCrossOrigin( this.crossOrigin );
+			loader.setPath( this.path );
 
-			if ( onLoad !== undefined ) {
+			loader.load( url, function ( img ) {
 
-				onLoad( texture );
+				texture.image = img;
 
-			}
+				// JPEGs can't have an alpha channel, so memory can be saved by storing them as RGB.
+				var isJPEG = url.search( /\.(jpg|jpeg)$/ ) > 0 || url.search( /^data\:image\/jpeg/ ) === 0;
 
-		}, onProgress, onError );
+				texture.format = isJPEG ? RGBFormat : RGBAFormat;
+				texture.needsUpdate = true;
+
+				if ( onLoad !== undefined ) {
+
+					onLoad( texture );
+
+				}
+
+			}, onProgress, onError );
+
+		} )
+		.catch( () => {
+
+			console.log( 'USING IMAGE' );
+
+			var loader = new ImageLoader( this.manager );
+			loader.setCrossOrigin( this.crossOrigin );
+			loader.setPath( this.path );
+
+			loader.load( url, function ( img ) {
+
+				texture.image = img;
+
+				// JPEGs can't have an alpha channel, so memory can be saved by storing them as RGB.
+				var isJPEG = url.search( /\.(jpg|jpeg)$/ ) > 0 || url.search( /^data\:image\/jpeg/ ) === 0;
+
+				texture.format = isJPEG ? RGBFormat : RGBAFormat;
+				texture.needsUpdate = true;
+
+				if ( onLoad !== undefined ) {
+
+					onLoad( texture );
+
+				}
+
+			}, onProgress, onError );
+
+		});
 
 		return texture;
 
