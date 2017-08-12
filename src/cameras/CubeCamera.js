a11a19912c14880e63dfda38eac7b2cec7bff3db
Jaume Sanchez <the.spite@gmail.com>
2017-08-12 02:55:40 +0200

    Changed format to support alpha

--- src/cameras/CubeCamera.js
+++ src/cameras/CubeCamera.js
@@ -49,7 +49,7 @@ function CubeCamera( near, far, cubeResolution ) {
 	cameraNZ.lookAt( new Vector3( 0, 0, - 1 ) );
 	this.add( cameraNZ );
 
-	var options = { format: RGBFormat, magFilter: LinearFilter, minFilter: LinearFilter };
+	var options = { format: RGBAFormat, magFilter: LinearFilter, minFilter: LinearFilter };
 
 	this.renderTarget = new WebGLRenderTargetCube( cubeResolution, cubeResolution, options );
 	this.renderTarget.texture.name = "CubeCamera";
